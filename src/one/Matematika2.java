01a6d035c826bde688aa65ed3ad920e38a56de31
wignn <tigfiragnafatur1933@gmail.com>
2024-08-31 23:14:44 +0700

    fist commit

---
+++ src/one/Matematika2.java
@@ -0,0 +1,8 @@
+package one;
+
+public class Matematika2 extends Matematika {
+    public void modulus(int a, int b) {
+        int r = a % b;
+        System.out.println("modulus :" + a + " / " + b + " = " + r);
+    }
+}
---
+++ src/two/KonversiSuhu.java
@@ -0,0 +1,13 @@
+package two;
+
+public class KonversiSuhu {
+    public void celciusToFahrenheit(double celcius) {
+        double fahrenheit = (celcius * 9 / 5) + 32;
+        System.out.println(celcius + " Celcius = " + fahrenheit + " Fahrenheit");
+    }
+
+    public void celciusToReamur(double celcius) {
+        double reamur = celcius * 4 / 5;
+        System.out.println(celcius + " Celcius = " + reamur + " Reamur");
+    }
+}
